2176e5c44c1ee5451c9ce00079c67e9b9529f37d
cereia <104402370+cereia@users.noreply.github.com>
2024-04-01 15:42:53 -0400

    Check every word in the string for dictionary word
    Substrings also removes nils and flattens the array for easy reducing

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,15 +1,22 @@
 def substrings(string, array)
   words_array = string.downcase.split
+  matching = words_array.map do |word| 
+    array.map { |dictionary_word| word[dictionary_word]}
+    .select { |word| word != nil}
+  end
+  .flatten
+  matching.reduce(Hash.new(0)) do |result, times|
+    result[times] += 1
+    result
+  end
 end
-
-
-
 
 
 # test cases
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 # ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-substrings("below", dictionary)
+p substrings("below", dictionary)
+
 # { "below" => 1, "low" => 1 }
-substrings("Howdy partner, sit down! How's it going?", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
 # { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
